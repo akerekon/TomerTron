dc767a04a84f67ca17c4e5d24a72bc1c46f85d51
akerekon <89589162+akerekon@users.noreply.github.com>
2024-01-02 22:01:07 -0500

    Add Flask and Prod Support

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
+"""Module allowing access to os environment variables."""
 import os
-from slack_bolt import App
+
 from dotenv import load_dotenv
+from flask import Flask, request
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
+from waitress import serve
 
+#Load in environment variables using dotenv, from a .env file
+#These secrets can be accessed by the current administrator of TomerTron via the Slack interface
 load_dotenv()
-
 app = App(
     token=os.getenv("SLACK_BOT_TOKEN"),
     signing_secret=os.getenv("SLACK_SIGNING_SECRET")
 )
 
-@app.message("hello")
+#In production, TomerTron uses Flask (and waitress) as a webserver
+#Reference https://slack.dev/bolt-python/concepts#adapters
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+@app.message("signoff")
 def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
+    """
+    Listen for messages containing the string "signoff"
+    """
     say(f"Hey there <@{message['user']}>!")
 
-# Start your app
+@app.event("message")
+def handle_message():
+    """
+    Ignore all messages not containing the above strings
+    """
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    """
+    Pass all requests under the path /slack/events to the events above
+    """
+    return handler.handle(request)
+
 if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+    #In testing, run with Flask using "flask run -p 3000"
+    #In production, serve our application using waitress using "python app.py"
+    serve(flask_app, host="127.0.0.1", port=3000)
