37e349251b957cc434000756f7b009909000d9d4
akerekon <89589162+akerekon@users.noreply.github.com>
2024-01-04 20:54:08 -0500

    Update registration

--- app.py
+++ app.py
@@ -86,8 +86,18 @@ def register_flow(ack, body, client):
     """
     command_flow.register_command(ack=ack, body=body, client=client)
 
+@app.command("tomertron register")
+def register_command(ack, body, client):
+    """
+    Begin the Slack account registration process when the "register" command is sent
+    """
+    command_flow.register_command(ack=ack, body=body, client=client)
+
 @app.view("registration-view")
 def register_submitted(ack, body, client, view, say):
+    """
+    Send the user a DM when they have successfully registered their account
+    """
     command_flow.register_submitted(ack=ack, body=body, client=client, view=view, say=say)
 
 @flask_app.route("/slack/events", methods=["POST"])
